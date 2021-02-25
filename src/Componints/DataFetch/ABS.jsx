4c9c533c76cd93c0e7690948612e56848230a13b
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-02-25 19:23:03 +0500

    Add featch

--- src/Componints/DataFetch/ABS.jsx
+++ src/Componints/DataFetch/ABS.jsx
@@ -6,34 +6,32 @@ function DataFetch () {
     "muhammadboburshoh", "omon490",
   ] 
   
-  const [data, setData] = useState({
+  const [data, setData] = useState([{
     loading: true,
     error: null, 
-    users: []
-  })
+    users: [],
+  }])
 
+  
 
 
   useEffect(() => {
 
-    userslogin.forEach(userLogin => {
 
-      fetch(`https://api.github.com/users/${userLogin}`)
+      fetch(`https://api.github.com/users`)
       .then(res => res.json())
       .then(user => {
-
-        console.log(user);
-        setData({ loading: false, users: [...data.users, user]})
+        // console.log(user);
+        setData({ loading: false, users: user})
       })
       .catch(error => {
         setData({error: "APIda xatolik bor."})
       })
 
-    })
 
   }, [])
 
-  // console.log(data.users);
+  console.log(data.users);
 
   return (
     <>
